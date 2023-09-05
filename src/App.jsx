7e1820da15583eed6cd30f77c1f9090f623cd71d
fatroo <rm552184@fiap.com.br>
2023-09-04 22:52:28 -0300

    css e app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-
+import Menu from './Components/menu'
+import Rodape from './Components/Rodape'
 export default function App() {
 
   return(
     <>
+      <Menu/>
       <Outlet/>
+      <Rodape/>
     </>
   )
 
